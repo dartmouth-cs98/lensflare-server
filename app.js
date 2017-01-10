fcd33b52f822eefc521beee8ea9cb95999d8ba6e
Jeremy <jeremy.c.mittleman.17@dartmouth.edu>
2017-01-09 19:36:32 -0500

    more work on producing signed s3 upload urls

--- app.js
+++ app.js
@@ -3,39 +3,37 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
-const aws = require('aws-sdk');
-const S3_BUCKET = process.env.S3_BUCKET;
 
 var USERS_COLLECTION = "users";
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.set('views', './views');
-app.listen(process.env.PORT || 3000);
-
 
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+var routes = require('./routes');
+app.use("/", routes);
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
 });
 
-require('./routes')(app);
+
 
--- routes.js
+++ routes.js
@@ -1,93 +1,99 @@
 const S3_BUCKET = process.env.S3_BUCKET;
 
-module.exports(app) {
-	// Generic error handler used by all endpoints.
-	function handleError(res, reason, message, code) {
-	  console.log("ERROR: " + reason);
-	  res.status(code || 500).json({"error": message});
-	}
-
-	// add a new user (as of now, JSON should include all object info)
-	app.post("/users", function(req, res) {
-		var newUser = req.body;
-		newUser.createDate = new Date();
-
-		// sanitize input
-		db.collection(USERS_COLLECTION).insertOne(newContact, function(err, doc) {
-		    if (err) {
-		      handleError(res, err.message, "Failed to create new user.");
-		    } else {
-		      res.status(201).json(doc.ops[0]);
-		    }
-	  	});
-
-	}); 
-
-	// get all users - will include display text? 
-	app.get("/users", function(req, res) {
-	  db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {
-	    if (err) {
-	      handleError(res, err.message, "Failed to get users.");
-	    } else {
-	      res.status(200).json(docs);
-	    }
-	  });
-	}); 
-
-	// Get a full user structure by ID
-	app.get("/users/:id", function(req, res) {
-		db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-		    if (err) {
-		      handleError(res, err.message, "Failed to get user");
-		    } else {
-		      res.status(200).json(doc);
-		    }
-		});
-	}); 
-
-	// delete a user
-	app.delete("/users/:id", function(req, res){
-	  db.collection(USERS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-	    if (err) {
-	      handleError(res, err.message, "Failed to delete contact");
-	    } else {
-	      res.status(204).end();
-	    }
-	  });
-	});
-
-
-	app.get("/sign-s3", function(req, res) {
-		const s3 = new aws.S3();
-		var return_list = [];
-		for (var image in res.image_infos) {
-			var imageName = image.name;
-			var fileType = image.type; 
-
-			const s3Params = {
-			    Bucket: S3_BUCKET,
-			    Key: imageName,
-			    Expires: 60,
-			    ContentType: fileType,
-			    ACL: 'public-read'
-			};
-
-		    s3.getSignedUrl('putObject', s3Params, (err, data) => {
-			    if(err){
-			      console.log(err);
-			      return res.end();
-			    }
-			    const returnData = {
-			      imageName: imageName,
-			      signedRequest: data,
-			      url: 'https://${S3_BUCKET}.s3.amazonaws.com/${fileName}'
-			    };
-			    return_list.push(returnData);
-			    // save it in the DB;
-		  	});
-
-		}
-		res.write(JSON.stringify(return_list));
-		res.end();
-	});
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
 }
+
+const util = require('util');
+const router = require('express').Router();
+const aws = require('aws-sdk');
+
+// module.exports()
+// {
+//     'use strict';
+// Generic error handler used by all endpoints.
+
+// add a new user (as of now, JSON should include all object info)
+router.post("/users", function (req, res) {
+    var newUser = req.body;
+    newUser.createDate = new Date();
+
+    // sanitize input
+    db.collection(USERS_COLLECTION).insertOne(newContact, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new user.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+
+});
+
+// get all users - will include display text?
+router.get("/users", function (req, res) {
+    db.collection(USERS_COLLECTION).find({}).toArray(function (err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get users.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+});
+
+// Get a full user structure by ID
+router.get("/users/:id", function (req, res) {
+    db.collection(USERS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get user");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
+});
+
+// delete a user
+router.delete("/users/:id", function (req, res) {
+    db.collection(USERS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete contact");
+        } else {
+            res.status(204).end();
+        }
+    });
+});
+
+router.get("/sign-s3", function (req, res) {
+    console.log("About to hit S3");
+    const s3 = new aws.S3();
+    var return_list = [];
+    console.log(req.body.image_infos);
+    req.body.image_infos.forEach(function (image) {
+        console.log("image is " + image);
+        var imageName = image.name;
+        var fileType = image.type;
+        var s3Params = {
+            Bucket: S3_BUCKET,
+            Key: imageName,
+            Expires: 60,
+            ContentType: fileType,
+            ACL: 'public-read'
+        };
+        s3.getSignedUrl('putObject', s3Params, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.end();
+            }
+            var returnData = {
+                imageName: imageName,
+                signedRequest: data,
+                url: util.format('https://%s.s3.amazonaws.com/%s', S3_BUCKET, imageName)
+        }
+            return_list.push(returnData);
+            // save it in the DB;
+        });
+    });
+    res.write(JSON.stringify(return_list));
+    res.end();
+});
+module.exports = router;
