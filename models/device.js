732b7e70f7635485e4d3f84d3c4090f1f7ea81dc
Jeremy <jeremy.c.mittleman.17@dartmouth.edu>
2017-02-14 16:43:30 -0500

    Add device registration collection + some routes

---
+++ models/device.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var deviceSchema = mongoose.Schema({
+    deviceName: {type: String, required: true},
+    spaceName: {type: String, required: true},
+    userEmail: {type: String, required: true}
+});
+
+deviceSchema.plugin(uniqueValidator);
+
+// necessary to use email to find user initially on login
+deviceSchema.statics.getDevice = function (deviceId, cb) {
+    return this.findOne({'deviceId': deviceId}, cb);
+};
+
+
+deviceSchema.statics.setSpace = function (deviceId, spaceName, cb) {
+    return this.findOne({'deviceId': deviceId}, (err, device) => {
+        device.spaceName = spaceName;
+        device.save((err) => {
+            cb(err);
+        });
+    });
+
+};
+
+
+module.exports = mongoose.model('Device', deviceSchema);
