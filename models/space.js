77635ed726b4a79f462d421637985fd498ff487c
Rich Shen <richard.d.shen@gmail.com>
2017-02-13 23:24:25 -0500

    mongoose schema revamp v1 (needs to be tested and debugged)

--- models/space.js
+++ models/space.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
-var Item = require('./item')
-var User = require('./user')
+var Item = require('./item.js')
+var User = require('./user.js')
 
 var spaceSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  items: { type: Array, 'default': [] }
+  items: [Item]
 });
 spaceSchema.plugin(uniqueValidator);
 
-spaceSchema.statics.getSpace = function (name, cb) {
-  return this.find({ name: new RegExp(name, 'i') }, cb);
-};
+// spaceSchema.statics.getSpace = function (name, cb) {
+//   return this.find({ name: new RegExp(name, 'i') }, cb);
+// };
 
 spaceSchema.statics.getItems = function (name, cb) {
   return this.find({ name: new RegExp(name, 'i') }, 'items -_id', cb);
